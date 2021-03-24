3a5ed2c423e4394601754793a0b06c41f84f651d
Brian-Munene <brianmunene69@gmail.com>
2021-03-24 13:31:27 +0300

    cors

--- public/index.php
+++ public/index.php
@@ -44,14 +44,6 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers:*");
-
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {//send back preflight request response
-return "";
-}
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
